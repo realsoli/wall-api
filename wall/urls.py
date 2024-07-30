a679d6bbb93c609e12241d30f9e7dec2c0c11580
realsoli <realsolii778@gmail.com>
2024-07-30 19:59:56 +0330

    modify Readme.me

--- wall/urls.py
+++ wall/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 urlpatterns = [
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/ads/', include('ads.urls')),
 ]
